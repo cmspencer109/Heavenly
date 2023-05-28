1dac780999c856505c2b008085b729e5a92a09c6
Christopher Spencer <cmspencer109@gmail.com>
2023-05-28 12:25:08 -0600

    WIP

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
+
 
 using namespace std;
 
@@ -104,50 +106,286 @@ std::vector<cv::Mat> resizeImages(const std::vector<cv::Mat>& images, double sca
 }
 
 
-int main() {
+cv::Mat differenceMask(const cv::Mat& image1, const cv::Mat& image2) {
+    // Check if the input images have the same size
+    if (image1.size() != image2.size()) {
+        std::cout << "Images must have the same size." << std::endl;
+        return cv::Mat();
+    }
+    
+    // Convert the input images to grayscale
+    cv::Mat image1Gray, image2Gray;
+    cv::cvtColor(image1, image1Gray, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(image2, image2Gray, cv::COLOR_BGR2GRAY);
+    
+    // Compute the absolute difference between the grayscale images
+    cv::Mat diff;
+    cv::absdiff(image1Gray, image2Gray, diff);
+    
+    // Apply a threshold to obtain a binary mask
+    // https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
+    cv::Mat mask;
+    cv::threshold(diff, mask, 10, 255, cv::THRESH_BINARY);
+    
+    // Apply Gaussian blur
+//  cv::GaussianBlur(mask, mask, cv::Size(0, 0), 2);
+    
+//  cv::threshold(mask, mask, 1, 255, cv::THRESH_BINARY);
+    
+//  cv::erode(mask, mask, cv::Mat(), cv::Point(-1, -1), 5);
+    
+    return mask;
+}
+
+
+cv::Mat starTrails(const std::vector<cv::Mat>& images) {
+    // Load the first image as the initial result
+    cv::Mat stackedImage = images[0];
+    
+    // Iterate over the remaining images
+    for (size_t i = 1; i < images.size(); ++i) {
+        cv::Mat nextImage = images[i];
+        stackedImage = blendLighten(stackedImage, nextImage);
+        std::cout << "Blended images " << i << " and " << i+1 << std::endl;
+    }
+    
+    return stackedImage;
+}
+
+cv::Mat contentAwareFill(const cv::Mat& stackedImage, const cv::Mat& mask, int radius)
+{
+    cv::Mat filledImage;
+    cv::inpaint(stackedImage, mask, filledImage, radius, cv::INPAINT_TELEA);
+    return filledImage;
+}
+
+
+std::vector<cv::Mat> maskImages(const std::vector<cv::Mat>& images, const cv::Mat& mask) {
+    // Check if the input vector of images is empty
+    if (images.empty()) {
+        std::cout << "No images provided." << std::endl;
+        return {};
+    }
+    
+    // Check if the mask image is empty or not in grayscale
+//  if (mask.empty() || mask.channels() != 1) {
+//      std::cout << "Invalid mask image." << std::endl;
+//      return {};
+//  }
+    
+    // Ensure that all images and the mask have the same size
+    cv::Size maskSize = mask.size();
+    for (const cv::Mat& image : images) {
+        if (image.size() != maskSize) {
+            std::cout << "All images must have the same size as the mask." << std::endl;
+            return {};
+        }
+    }
+    
+    // Convert the mask to binary (threshold it)
+    cv::Mat maskBinary;
+    cv::threshold(mask, maskBinary, 1, 255, cv::THRESH_BINARY);
+    
+    // Apply the mask to each image
+    std::vector<cv::Mat> maskedImages;
+    for (const cv::Mat& image : images) {
+        cv::Mat maskedImage;
+        image.copyTo(maskedImage, maskBinary);
+        maskedImages.push_back(maskedImage);
+    }
+    
+    return maskedImages;
+}
+
+
+cv::Mat makePanorama(const std::vector<cv::Mat>& images) {
+    cv::Mat pano;
+    cv::Ptr<cv::Stitcher> stitcher = cv::Stitcher::create(cv::Stitcher::PANORAMA);
+    cv::Stitcher::Status status = stitcher->stitch(images, pano);
+    
+    if (status != cv::Stitcher::OK)
+    {
+        cout << "Can't stitch images, error code = " << int(status) << endl;
+//      return EXIT_FAILURE;
+    }
+    
+    return pano;
+}
+
+
+
+//int main() {
+int main(int argc, char** argv) {
     /*
     cd build
     make && ./Heavenly
     */
     
-    std::vector<std::string> imagePaths = {
-        "../images/_MG_8206.jpg",
-        "../images/_MG_8207.jpg",
-        "../images/_MG_8208.jpg",
-        "../images/_MG_8209.jpg",
-        "../images/_MG_8210.jpg",
-        "../images/_MG_8211.jpg",
-        "../images/_MG_8212.jpg",
-        "../images/_MG_8213.jpg",
-        "../images/_MG_8214.jpg",
-        "../images/_MG_8215.jpg",
-    };
+//  std::vector<std::string> imagePaths = {
+//      "../images/_MG_8206.jpg",
+//      "../images/_MG_8207.jpg",
+//      "../images/_MG_8208.jpg",
+//      "../images/_MG_8209.jpg",
+//      "../images/_MG_8210.jpg",
+//      "../images/_MG_8211.jpg",
+//      "../images/_MG_8212.jpg",
+//      "../images/_MG_8213.jpg",
+//      "../images/_MG_8214.jpg",
+//      "../images/_MG_8215.jpg",
+//  };
     
-    std::vector<cv::Mat> images = resizeImages(convertToCVMat(imagePaths), 0.5);
+//  std::vector<cv::Mat> images = resizeImages(convertToCVMat(imagePaths), 0.5);
+//  std::vector<cv::Mat> images = resizeImages(convertToCVMat(imagePaths), 1);
     
-    cv::Mat resultImage;
     
-    resultImage = stackModeMedian(images);
-    
-//  // Load the first image as the initial result
-//  cv::Mat currentImage = cv::imread(imagePaths[0]);
+//  //  Create clean sky
+//  cv::Mat cleanSkyImage = stackModeMedian(images);
 //  
-//  // Iterate over the remaining images
-//  for (size_t i = 1; i < imagePaths.size(); ++i) {
-//      cv::Mat nextImage = cv::imread(imagePaths[i]);
-//      currentImage = blendLighten(currentImage, nextImage);
-//      std::cout << "Blended images " << i << " and " << i+1 << std::endl;
+//  cv::imwrite("clean_sky.jpg", cleanSkyImage);
+//  
+//  cv::imshow("Blended Image", cleanSkyImage);
+//  cv::waitKey(0);
+//  
+//  //  Create star trails
+//  cv::Mat starrySkyImage = starTrails(images);
+//  
+//  cv::imwrite("starry_sky.jpg", starrySkyImage);
+//  
+//  cv::imshow("Blended Image", starrySkyImage);
+//  cv::waitKey(0);
+    
+    // Create mask from differences
+//  cv::Mat cleanSkyImage = cv::imread("../clean_sky_small.jpg");
+//  cv::Mat starrySkyImage = cv::imread("../starry_sky_small.jpg");
+//  cv::Mat mask = differenceMask(cleanSkyImage, starrySkyImage);
+    
+//  cv::imwrite("mask.jpg", mask);
+    
+//  cv::imshow("Changes Mask", mask);
+//  cv::waitKey(0);
+//  
+    // Fill star trail gaps
+//  cv::Mat filledImage = contentAwareFill(starrySkyImage, mask, 3);
+//  
+//  cv::imwrite("../output.jpg", filledImage);
+//  
+//  cv::imshow("Star trails", cv::imread("../starry_sky_small.jpg"));
+//  cv::waitKey(0);
+//  
+//  cv::imshow("Filled trails", filledImage);
+//  cv::waitKey(0);
+    
+    
+//  ########### LSD DETECTION ###############
+//  cv::Mat input = cv::imread("../starry_sky_small.jpg");
+//  cv::Mat input = cv::imread("../test.jpg");
+//  cv::Mat gray;
+//  cv::cvtColor(input, gray, cv::COLOR_BGRA2GRAY);
+//  
+//  cv::Ptr<cv::LineSegmentDetector> det;
+//  det = cv::createLineSegmentDetector();
+//  
+//  cv::Mat lines;
+//  det->detect(gray, lines);
+//  
+//  det->drawSegments(input, lines);
+//  
+//  cv::imshow("input", input);
+//  cv::waitKey(0);
+    
+    
+    
+    
+//  cv::Mat inputImage = cv::imread("../test.jpg");
+//  cv::Mat inputImageGray;
+//  cv::cvtColor(inputImage, inputImageGray, cv::COLOR_BGR2GRAY);
+//  
+//  cv::imshow("", inputImageGray);
+//  cv::waitKey(0);
+//  
+//  cv::Mat edges;
+//  cv::Canny(inputImageGray, edges, 150, 200, 3);
+//  
+//  int minLineLength = 30;
+//  int maxLineGap = 5;
+//  
+//  std::vector<cv::Vec4i> lines;
+//  cv::HoughLinesP(edges, lines, CV_PI / 180, 30, minLineLength, maxLineGap);
+//  
+//  for (size_t i = 0; i < lines.size(); i++) {
+//      cv::Vec4i line = lines[i];
+//      cv::line(inputImage, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
 //  }
 //  
-//  resultImage = currentImage;
+//  cv::putText(inputImage, "Tracks Detected", cv::Point(500, 250), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+//  
+//  cv::imshow("result", inputImage);
+//  cv::waitKey(0);
+//  cv::imshow("edge", edges);
+//  cv::waitKey(0);
+//  
+    
+    
+    std::vector<cv::Mat> images = {
+        cv::imread("../images/milkyway/_MG_3534.jpg"),
+        cv::imread("../images/milkyway/_MG_3535.jpg"),
+        cv::imread("../images/milkyway/_MG_3536.jpg"),
+        cv::imread("../images/milkyway/_MG_3537.jpg"),
+        cv::imread("../images/milkyway/_MG_3538.jpg"),
+        cv::imread("../images/milkyway/_MG_3539.jpg"),
+        cv::imread("../images/milkyway/_MG_3540.jpg"),
+        cv::imread("../images/milkyway/_MG_3541.jpg"),
+        cv::imread("../images/milkyway/_MG_3542.jpg"),
+        cv::imread("../images/milkyway/_MG_3543.jpg"),
+    };
+    
+    std::vector<cv::Mat> masks = {
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+        cv::imread("../images/milkyway/mask.jpg"),
+    };
+    
+    
+    
+    
+//  std::vector<cv::Mat> p = {
+//      cv::imread("../images/p/_MG_8739.jpg"),
+//      cv::imread("../images/p/_MG_8740.jpg"),
+//      cv::imread("../images/p/_MG_8741.jpg"),
+//  };
+    
+//  cv::Mat pano = makePanorama(p);
+//  
+//  cv::imwrite("pano.jpg", pano);
+//  
+//  cv::imshow("", pano);
+//  cv::waitKey(0);
 
-    cv::imshow("Blended Image", resultImage);
-    cv::waitKey(0);
+    
+    // Iterate over the aligned images and display each one
+//  for (size_t i = 0; i < alignedImages.size(); ++i)
+//  {
+//      cv::imshow("Aligned Image " + std::to_string(i+1), alignedImages[i]);
+//      cv::waitKey(0);
+//  }
+    
+    
+//  cv::Mat res = stackModeMedian(alignedImages);
+//  
+//  cv::imshow("", res);
+//  cv::waitKey(0);
     
     // Save the result image to a file
-    std::string outputImagePath = "output_image.jpg";
-    cv::imwrite(outputImagePath, resultImage);
-    std::cout << "Blended image saved to: " << outputImagePath << std::endl;
+//  std::string outputImagePath = "output_image.jpg";
+//  cv::imwrite(outputImagePath, resultImage);
+//  std::cout << "Blended image saved to: " << outputImagePath << std::endl;
     
 
     return 0;
